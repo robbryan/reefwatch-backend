cd22d312cfd1c0b3f83b715addc5966ca0fefeed
Teh Stasz <stasz-coursera@mailismagic.com>
2016-04-17 13:05:57 +0930

    Intitial 'Hello, World' service worning with command-line options

---
+++ app.py
@@ -0,0 +1,54 @@
+"""
+    This is the main application file from which the ReefWatch backend is started
+"""
+
+__author__    = "Paul Staszyc"
+__copyright__ = "Copyright 2016"
+
+
+import tornado.ioloop
+import tornado.web
+import tornado.httpserver
+from tornado.options import define, options
+
+import logging
+
+
+if not hasattr(options, "log_file"):
+    define("log_file", default="access.log", help="Name of file to which to log")
+
+logging.basicConfig(filename=options.log_file, format='%(asctime)-6s: %(name)s - %(levelname)s - %(message)s')
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world!")
+
+
+class Application(tornado.web.Application):
+
+    logger = logging.getLogger('ReefWatch Backend Event logger')
+
+    def __init__(self):
+        settings = dict(
+            debug=options.debug if 'debug' in options else False
+        )
+
+        handlers = [
+            (r"/", MainHandler),
+        ]
+
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+def main():
+    define("port", default="9880", help="Port on which the HTTP server will listen")
+    define("debug", default=False, help="Set some logging options + auto-reload on file change")
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
