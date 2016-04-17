95fa70c1d9fd111fc2e8ce0b2104f1d5463982da
pablo-libre <stasz-coursera@mailismagic.com>
2016-04-17 21:57:26 +0930

    Added Authenticated base handler

--- baseHandler.py
+++ baseHandler.py
@@ -7,6 +7,7 @@ __copyright__ = "Copyright 2016"
 
 
 import tornado.web
+import tornado.escape
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -14,5 +15,18 @@ class BaseHandler(tornado.web.RequestHandler):
     """ This will be used for functions which require a user context - most, if not all of them will """
     def get_current_user(self):
         user_json = self.get_secure_cookie("user")
-        if not user_json: return None
+        if not user_json:
+            return None
+
         return tornado.escape.json_decode(user_json)
+
+
+class BaseAuthenticatedHandler(BaseHandler):
+
+    __user_id__ = None
+
+    @tornado.web.authenticated
+    def prepare(self):
+
+        if "user_id" in self.current_user:
+            self.__user_id__ = self.current_user["user_id"]
