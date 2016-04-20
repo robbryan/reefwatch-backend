c9490a89b21e11ca1c37284fc84915b6c359fecb
Paul <paul@natalia.(none)>
2016-04-20 10:10:27 +0930

    Limit and offset implemented for Site

--- siteHandler.py
+++ siteHandler.py
@@ -6,17 +6,21 @@ __author__    = "Paul Staszyc"
 __copyright__ = "Copyright 2016"
 
 
-from baseHandler import BaseHandler
+from baseHandler import BaseEntityListHandler
 
 
-class SiteListHandler(BaseHandler):
+class SiteListHandler(BaseEntityListHandler):
 
     def initialize(self, persistentEntityListObj):
         self.__persistentEntityListObj__ = persistentEntityListObj
 
     def get(self, location=None):
+        limit, offset = self.getLimitAndOffset()
         entityListGetter = self.__persistentEntityListObj__
-        entityList, totalRecordCount = entityListGetter.get()
+        entityList, totalRecordCount = entityListGetter.get(
+            limit=limit,
+            offset=offset
+            )
         self.write({"data": entityList})
 
 
