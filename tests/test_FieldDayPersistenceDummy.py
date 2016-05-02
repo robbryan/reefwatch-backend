50fd2e313ea201e23377ada27c85f7907c3bc5d4
pablo-libre <pstaszyc+github@gmail.com>
2016-05-02 23:56:39 +0930

    Clean up tests

--- tests/test_FieldDayPersistenceDummy.py
+++ tests/test_FieldDayPersistenceDummy.py
@@ -14,9 +14,9 @@ class TestReefwatchFieldDay(tornado.testing.AsyncTestCase):
         print "Teardown"
 
     @tornado.testing.gen_test
-    def test_basic(self):
+    def test_get_by_id(self):
         dummyEntityGetter = EntityDummy()
         dummyEntity = yield dummyEntityGetter.get(fieldDayId="90e49f7a-087b-11e6-ad28-902b34626bbb")
         self.assertIsInstance(dummyEntity, dict)
         self.assertTrue(any(dummyEntity))
-        
+
