d9245d402c6c36b2e928ce3ddf254f21a37feba1
pablo-libre <pstaszyc+github@gmail.com>
2016-05-20 13:51:24 +0930

    Field Day Surveys ready to move under Field Day Sites - but currently incorrectly placed directly under Field Day

--- surveyHandler.py
+++ surveyHandler.py
@@ -5,11 +5,16 @@
 __author__    = "Paul Staszyc"
 __copyright__ = "Copyright 2016"
 
+from tornado.gen import coroutine
 
-from baseHandler import BaseHandler
+from baseHandler import BaseEntityListHandler
+
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logger.getEffectiveLevel())
 
 
-class SurveyListHandler(BaseHandler):
+class SurveyTypeListHandler(BaseEntityListHandler):
 
     def initialize(self, persistentSurveyListObj):
         self.__persistentSurveyListObj__ = persistentSurveyListObj
@@ -18,6 +23,49 @@ class SurveyListHandler(BaseHandler):
         surveyListGetter = self.__persistentSurveyListObj__
         surveyList = surveyListGetter.get()
         self.write({"data": surveyList})
+
+
+class FieldDaySurveyListHandler(BaseEntityListHandler):
+
+    def initialize(self, persistentFieldDaySurveyListObj):
+        self.__persistentFieldDaySurveyListObj__ = persistentFieldDaySurveyListObj
+
+    def options(self, *args):
+        allowedMethods = list(["OPTIONS", "GET"])
+        try:
+            user = self.get_current_user()
+            if user and any(user):
+                pass #allowedMethods.extend(["POST"]) # PUT and DELETE to follow
+        except Exception as ex:
+            logger.error(ex)
+
+        self.set_header("Access-Control-Allow-Methods", ",".join(allowedMethods))
+        self.finish()
+
+    @coroutine
+    def get(self, fieldDayId):
+        try:
+            pageSize, pageNum = self.getPageSizeAndNum()
+        except ValueError as exPage:
+            self.set_status(400)
+            self.add_header("error", "{0}".format(exPage))
+            self.finish({"message": "{0}".format(exPage)})
+            return
+
+        offset = (pageNum-1)*pageSize
+        limit = pageSize
+        entityListGetter = self.__persistentFieldDaySurveyListObj__
+        fieldDaySurveyList, totalRecordCount = yield entityListGetter.get(
+            fieldDayId=fieldDayId,
+            limit=limit,
+            offset=offset
+            )
+        self.setResponseHeadersList(
+            pageNum=pageNum,
+            pageSize=pageSize,
+            totalRecordCount=totalRecordCount
+        )
+        self.finish({"data": fieldDaySurveyList})
 
 
 if __name__ == "__main__":
