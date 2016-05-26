cd275eeae8fb94de52d6058376d012ad83f4d8b3
pablo-libre <pstaszyc+github@gmail.com>
2016-05-26 20:22:08 +0930

    Remove Dummy Persistence Survey. Add Peristence Survey Type base

--- persistence/SurveyPersistenceBase.py
+++ persistence/SurveyPersistenceBase.py
@@ -5,25 +5,31 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class PersistentSurveyBase(object):
+class PersistentSurveyTypeBase(object):
 
     def __init__(self):
         logger.warning("Abstract class created")
         pass
 
-    def save(self):
+    def get(self):
+        raise NotImplementedError
+
+    def add(self):
+        raise NotImplementedError
+
+    def update(self):
         raise NotImplementedError
 
     def delete(self):
         raise NotImplementedError
 
 
-class PersistentServeyDummy(PersistentSurveyBase):
+class PersistentServeyTypeDummy(PersistentSurveyTypeBase):
 
     def __init__(self):
         pass
 
-    def save(self):
+    def get(self):
         pass
 
 if __name__ == "__main__":
